a868c5fd1fd8720657333f365d435a6fd1f45446
aragasa <58983597+aragasa@users.noreply.github.com>
2020-03-11 22:02:44 -0400

    Update ConfigServiceApplication.java
    added the documentation

--- config-service/src/main/java/com/rms/ConfigServiceApplication.java
+++ config-service/src/main/java/com/rms/ConfigServiceApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+*@author 1912dec16 Fullstack Java
+*<br>
+*<br>
+*The Config server pulls down the configuration files from one of our personal 
+*git repository and distributes them to all of the services that ask for them.
+*/
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
